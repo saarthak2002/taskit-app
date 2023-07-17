c0a4064df83c6bcd5d511cbba9f7f7316a3754a4
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-07-17 19:30:43 +0530

    integrated backend REST api to store user info

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Button from '@mui/material/Button';
 
 import { auth } from '../firebase-config';
 import { signOut, onAuthStateChanged } from "firebase/auth";
 
+import axios from "axios";
+
 const Home = () => {
 
     const [user, setUser] = useState({});
+    const [userInfo, setUserInfo] = useState({});
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
+                console.log(user.uid);
                 setUser(user);
+                axios
+                    .get(process.env.REACT_APP_API_URI + 'users/' + user.uid)
+                    .then((response) => {
+                        console.log(response.data);
+                        setUserInfo(response.data);
+                    })
+                    .catch((error) => {
+                        console.log('error:' + error);
+                    });
             } else {
                 setUser(null);
+                setUserInfo(null);
             }
           });
     }, []);
@@ -34,8 +48,8 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
-            <p>This is the home page</p>
-            {user ? user.email && <h5>{user.email}</h5> : <h5>No user logged in</h5>}
+            {userInfo && <h2>Hello, {userInfo?.firstname + ' ' + userInfo?.lastname}</h2>}
+            {user ? user.email && <h5>{user.email} {user.uid}</h5> : <h5>No user logged in</h5>}
             {
                 user?
                     <Button variant="contained" onClick={handleLogout}>Logout</Button>
--- src/components/Register.js
+++ src/components/Register.js
@@ -11,6 +11,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { auth } from "../firebase-config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
+import axios from "axios";
+
 const defaultTheme = createTheme();
 
 const Register = () => {
@@ -19,13 +21,34 @@ const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [userName, setUserName] = useState('');
 
     const handleRegister = async () => {
-        console.log(email, password);
+        console.log(email, password, userName, firstName, lastName);
 
         try {
             const user = await createUserWithEmailAndPassword(auth, email, password);
-            console.log(user);
+            
+            var bodyFormData = new FormData();
+            bodyFormData.append('userUID', auth.currentUser.uid);
+            bodyFormData.append('username', userName);
+            bodyFormData.append('firstName', firstName);
+            bodyFormData.append('lastName', lastName);
+            
+            const headers = {
+                'Content-Type': 'application/json',
+            }
+
+            axios
+                .post(process.env.REACT_APP_API_URI + 'users', bodyFormData, { headers: headers })
+                .then((response) => {
+                    console.log(response.data.message);
+                })
+                .catch((error) => {
+                    console.log('error:' + error);
+                });
             navigate('/');
         }
 
@@ -44,6 +67,11 @@ const Register = () => {
                 <Stack 
                     spacing={2}
                 >
+                    <div>
+                        <TextField id="first-name" label="First name" variant="outlined" onChange={ (event) => setFirstName(event.target.value) }/>
+                        <TextField id="last-name" label="Last name" variant="outlined" onChange={ (event) => setLastName(event.target.value) }/>
+                    </div>
+                    <TextField id="username" label="Username" variant="outlined" onChange={ (event) => setUserName(event.target.value) }/>
                     <TextField id="email" label="Email" variant="outlined" onChange={ (event) => setEmail(event.target.value) }/>
                     <TextField id="password" label="Password" variant="outlined" onChange={ (event) => setPassword(event.target.value) }/>
                     <TextField id="confirm-password" label="Confirm password" variant="outlined" onChange={ (event) => setConfirmPassword(event.target.value) }/>
