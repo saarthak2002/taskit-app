96d632ae5bccec7c68d5ec7af805389a95490af0
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-07-20 14:59:06 +0530

    added edit category feature

--- src/components/EditTaskModal.js
+++ src/components/EditTaskModal.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 import Typography from '@mui/material/Typography';
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import TextField from '@mui/material/TextField';
 import axios from "axios";
 import CircularProgress from '@mui/material/CircularProgress';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
 
 
 const EditTaskModal = (props) => {
@@ -14,6 +19,52 @@ const EditTaskModal = (props) => {
     const [taskTitle, setTaskTitle] = useState(task.title);
     const [taskDesc, setTaskDesc] = useState(task.description);
     const [modalLoading, setModalLoading] = useState(false);
+    const [categories, setCategories] = useState([]);
+
+    const [taskCategoryId, setTaskCategoryId] = useState(-1);
+    const [taskCategoryName, setTaskCategoryName] = useState("None");
+    const [taskCategoryColor, setTaskCategoryColor] = useState("#bab5b5");
+
+    const getCategories = useCallback(() => {
+        setModalLoading(true);
+        axios
+            .get(process.env.REACT_APP_API_URI + 'taskcategories/project/' + task.project_id)
+            .then((response) => {
+                console.log(response.data);
+                setCategories(response.data);
+                response.data.forEach(category => {
+                    if(category.name === task.task_category_name) {
+                        setTaskCategoryId(category.id);
+                        setTaskCategoryName(category.name);
+                        setTaskCategoryColor(category.color);
+                    }
+                });
+                setModalLoading(false);
+            })
+            .catch((error) => {
+                console.log('error:' + error);
+                alert('error:' + error);
+                setModalLoading(false);
+            })
+    }, [task.project_id, task.task_category_name]);
+
+    const handleCategoryChange = (event) => {
+        if (event.target.value === "-1") {
+            console.log("None");
+            setTaskCategoryName("None");
+            setTaskCategoryColor("#bab5b5");
+        }
+        setTaskCategoryId(event.target.value);
+        categories.forEach(category => {
+            if (category.id === event.target.value) {
+                setTaskCategoryName(category.name);
+                setTaskCategoryColor(category.color);
+                console.log(category.name);
+                console.log(category.color);
+            }
+        });
+        console.log(event.target.value);
+    };
 
     const updateTask = () => {
         setModalLoading(true);
@@ -25,6 +76,8 @@ const EditTaskModal = (props) => {
         var updateData = new FormData();
         updateData.append('title', taskTitle);
         updateData.append('description', taskDesc);
+        updateData.append('task_category_name', taskCategoryName);
+        updateData.append('task_category_color', taskCategoryColor);
 
         axios
             .post(process.env.REACT_APP_API_URI + 'task/' + task.id + '/edit', updateData, { headers: headers })
@@ -41,6 +94,10 @@ const EditTaskModal = (props) => {
             });
     }
 
+    useEffect(() => {
+        getCategories();
+    }, [getCategories]);
+
     return (
         <div>
             {
@@ -54,6 +111,32 @@ const EditTaskModal = (props) => {
                             Edit Task
                         </Typography>
                         <TextField id="title" label="Title" variant="outlined" onChange={ (event) => { setTaskTitle(event.target.value) } } style={{width:'100%', paddingBottom:'4%'}} defaultValue={task.title}/>
+
+                        <Box sx={{ minWidth: 100, paddingBottom:'3%' }}>
+                            <FormControl fullWidth>
+                                    <div style={{ width: '100%' }}>
+                                        <InputLabel id="select-label" >Category</InputLabel>
+                                        <Select
+                                            sx={{ width: '100%' }}
+                                            labelId="select-label"
+                                            id="simple-select"
+                                            value={taskCategoryId}
+                                            label="Category"
+                                            onChange={ (e) => {handleCategoryChange(e)} }
+                                        >
+                                            <MenuItem value="-1">None</MenuItem>
+                                            {
+                                                categories.map(category => {
+                                                    return (
+                                                        <MenuItem key={category.id} value={category.id} >{category.name}</MenuItem>
+                                                    )
+                                                })
+                                            }
+                                        </Select>
+                                    </div>
+                            </FormControl>
+                        </Box>
+
                         <TextField id="desc" label="Description" variant="outlined" multiline rows={3} onChange={ (event) => { setTaskDesc(event.target.value) } } style={{width:'100%', paddingBottom:'4%'}} defaultValue={task.description} />
                         
                         <Stack direction="row" spacing={2}>
